03a3c2de7a7734a77a456ef3a4bedcc613d93554
Sanskar-12 <sanskarv2004@gmail.com>
2025-05-27 14:58:02 +0530

    prisma adapter and connection with db

---
+++ auth.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+});
